dc61fb2f60c918947dcc56d5eec389a4c1135ce1
michael eschbacher <michaeleschbacher@gmail.com>
2014-03-26 19:32:14 -0500

    Pass all of the tests

--- LineDrawer.php
+++ LineDrawer.php
@@ -23,19 +23,42 @@ class LineDrawer
     }
     protected function render($str)
     {
-        if (preg_match_all('%(.+?):(\d+)%', $str, $matches,2)) {
-            $ret = '';
-            foreach ($matches as $match) {
-            $ret.=str_repeat(str_replace(array_keys($this->map), array_values($this->map), $match[1]), $match[2]);
-            }
-            return $ret;
+        while (preg_match('%^(.+?)(\d+)(.*)$%', $str, $match)) {
+
+            $last_slash = strrpos($match[1], '/');
+            if ($last_slash === false) {
+                // Only repeat the last character
+                $str = substr($match[1], 0, -1);
+                $str.= str_repeat(substr($match[1], -1) , $match[2]);
+                $str.=$match[3];
+                    //$str = substr($match[1], 0, -2) . str_repeat(substr($match[1],-1), $match[2]) . $match[3] ."\n";
+            } else {
+//                echo $last_slash;
+                $str = substr($match[1], 0, $last_slash);
+                $str.= str_repeat(substr($match[1], $last_slash) , $match[2]);
+                $str.=$match[3];
+               
+            } 
+            //print_r($match);
+//            echo 'orig:' . $str;
+//            echo "\n";
+          //  $str = str_repeat($match[1], $match[2]) .$match[3];
+//    echo $str;
+    //break;
+            //    echo $str;
+          //  die();
         }
-        return str_replace(array_keys($this->map), array_values($this->map), $str);
-        echo "\nrender this...\n";
-        echo $str . "\n";
+        $str = str_replace(array_keys($this->map), array_values($this->map), $str);
+        return str_replace('/', '', $str);
+        return $str;
+        die();
+        
     }
     public function map($k, $v)
     {
+        if (isset($k[1]) && $k[0] != '/') {
+            $k = '/' . $k;
+        }
         $this->map[$k] = $v;
     }
 }
