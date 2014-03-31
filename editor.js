cc3da6722043fccae028c74b8dfc7924e67b5442
Micah <micah@whamdonk.com>
2014-03-31 13:25:51 -0500

    Fill out the new form elements

--- editor.js
+++ editor.js
@@ -1,7 +1,15 @@
 var pubsub = _.extend(Backbone.Events);
-console.log(pubsub);
 
-var Space = Backbone.Model.extend();
+
+var Space = Backbone.Model.extend({
+    defaults : {
+        type : 'other'
+    }
+});
+Space.A = function (v) {
+    return v.replace(/[^A-Z]/g,'').substr(0,2);
+    console.log(v);
+}
 var SpaceCollection = Backbone.Collection.extend({model:Space});
 
 var SpaceListingView = Backbone.View.extend({
@@ -23,18 +31,31 @@ var App = Backbone.View.extend({
     var that = this;
         $.getJSON('spaces.json', function (json) {
             that.collection = new SpaceCollection(json.spaces);
-//            for(var i =0; i < json.spaces.length; i++) {
-            
-  //          }
-    //        console.log(json);
             that.render();
         });
 
     },
+    events : {
+        'change #type' : 'typeChangeHandler',
+        'blur #name' : 'nameBlurHandler'
+    },
+    nameBlurHandler : function (evt) {
+        if (this.$el.find('#abbr').val().length > 0) {
+            return;
+        }
+        this.$el.find('#abbr')[0].value = Space.A(evt.target.value);
+        console.log('we need to generate an abbr for ' + evt.target.value);
+    },
+    typeChangeHandler : function (evt) {
+        var method = evt.target.value === 'property' ? 'show': 'hide';
+        this.$el.find('#price').closest('.form-group')[method]();
+    },
     loadForm : function (model) {
-
+        this.$el.find('#name').val(model.get('name'));;
         this.$el.find('#abbr').val(model.get('abbr'));
-
+        this.$el.find('#price').val(model.get('price'));
+        this.$el.find('#color').val(model.get('color'));
+        this.$el.find('#type').val(model.get('type'));
     },
     render : function () {
         var $body= this.$el.find('.list').empty();
